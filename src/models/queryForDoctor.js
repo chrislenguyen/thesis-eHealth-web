ef7e3b7a0cb2256fc80ddd01ae031573b7148d10
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2021-03-18 17:38:56 +0700

    change query and returned result

--- src/models/queryForDoctor.js
+++ src/models/queryForDoctor.js
@@ -3,16 +3,16 @@ const mysql = require("mysql");
 
 const queryForDoctor = (doctorUsername, callback) => {
 	const query =
-		"SELECT DOCTOR.First_Name as fname, DOCTOR.Last_Name as lname, EXAM_ROOM.Building_Code as buildingCd, EXAM_ROOM.Exam_Room_Code as examRoomCd, HOSPITAL.Hospital_Name as hosName FROM DOCTOR INNER JOIN EXAM_ROOM ON DOCTOR.Dep_ID = EXAM_ROOM.Dep_ID AND DOCTOR.Hospital_ID = EXAM_ROOM.Hospital_ID    INNER JOIN HOSPITAL   ON EXAM_ROOM.Hospital_ID = HOSPITAL.Hospital_ID WHERE DOCTOR.USER_NAME = " +
+		"SELECT DISTINCT doctor.First_Name fname, doctor.Last_Name lname, hospital.Hospital_ID hosId, hospital.Hospital_Name hosName, building.Building_Code bCode, exRoom.Exam_Room_Code exRoomCode FROM DOCTOR doctor LEFT JOIN HOSPITAL hospital ON  DOCTOR.Hospital_ID = hospital.Hospital_ID LEFT JOIN BUILDING building on hospital.Hospital_ID = building.Hospital_ID LEFT JOIN EXAM_ROOM exRoom ON hospital.Hospital_ID = exRoom.Hospital_ID AND building.Building_Code = exRoom.Building_Code WHERE doctor.USER_NAME =" +
 		mysql.escape(doctorUsername) +
-		" FOR JSON AUTO";
-        // console.log(query);
+		"AND doctor.Dep_ID = exRoom.Dep_ID FOR JSON AUTO";
+	// console.log(query);
 	queryForObject(query, (err, data) => {
 		if (err) {
 			callback(err, undefined);
 		} else {
 			// console.log(data);
-			callback(undefined, data);
+			callback(undefined, data[0]);
 		}
 	});
 };
