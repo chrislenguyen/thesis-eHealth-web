06889959a12142c9edfda12806add93739020cbb
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2021-03-21 21:05:46 +0700

    fix query get doctor ID

--- src/models/queryForDoctor.js
+++ src/models/queryForDoctor.js
@@ -3,10 +3,9 @@ const mysql = require("mysql");
 
 const queryForDoctor = (doctorUsername, callback) => {
 	const query =
-		"SELECT DISTINCT doctor.First_Name fname, doctor.Last_Name lname, hospital.Hospital_ID hosId, hospital.Hospital_Name hosName, building.Building_Code bCode, exRoom.Exam_Room_Code exRoomCode FROM DOCTOR doctor LEFT JOIN HOSPITAL hospital ON  DOCTOR.Hospital_ID = hospital.Hospital_ID LEFT JOIN BUILDING building on hospital.Hospital_ID = building.Hospital_ID LEFT JOIN EXAM_ROOM exRoom ON hospital.Hospital_ID = exRoom.Hospital_ID AND building.Building_Code = exRoom.Building_Code WHERE doctor.USER_NAME =" +
+		"SELECT DISTINCT doctor.Doctor_ID docId, doctor.First_Name fname, doctor.Last_Name lname, hospital.Hospital_ID hosId, hospital.Hospital_Name hosName, building.Building_Code bCode, exRoom.Exam_Room_Code exRoomCode FROM DOCTOR doctor LEFT JOIN HOSPITAL hospital ON  DOCTOR.Hospital_ID = hospital.Hospital_ID LEFT JOIN BUILDING building on hospital.Hospital_ID = building.Hospital_ID LEFT JOIN EXAM_ROOM exRoom ON hospital.Hospital_ID = exRoom.Hospital_ID AND building.Building_Code = exRoom.Building_Code WHERE doctor.USER_NAME =" +
 		mysql.escape(doctorUsername) +
 		"AND doctor.Dep_ID = exRoom.Dep_ID FOR JSON AUTO";
-	// console.log(query);
 	queryForObject(query, (err, data) => {
 		if (err) {
 			callback(err, undefined);
