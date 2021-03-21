69e4ca5c5cea119fabf51ac62c739a4754c2279f
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2021-03-21 21:13:39 +0700

    create add new patient to DB module

---
+++ src/models/queryAddNewPatient.js
@@ -0,0 +1,63 @@
+const queryForObject = require("./queryForObject");
+const mysql = require("mysql");
+const md5 = require("md5");
+
+const queryAddNewPatient = (
+	{ pId, fname, lname, dob, gender, address, phoneNo, ssn } = {},
+	callback
+) => {
+	const query =
+		"DECLARE @return_status INT " +
+		"EXEC hospital.Update_Patient_Infor " +
+		"@Patient_ID = " +
+		mysql.escape(pId) +
+		" ," +
+		"@First_Name =" +
+		mysql.escape(fname) +
+		"," +
+		"@Last_Name =" +
+		mysql.escape(lname) +
+		"," +
+		"@Date_Of_Birth =" +
+		mysql.escape(dob) +
+		"," +
+		"@Gender =" +
+		mysql.escape(gender) +
+		"," +
+		"@Address =" +
+		mysql.escape(address) +
+		"," +
+		"@Phone_Number =" +
+		mysql.escape(phoneNo) +
+		"," +
+		"@SSN =" +
+		mysql.escape(ssn) +
+		"," +
+		"@User_Name =" +
+		mysql.escape(
+			fname.toLowerCase() +
+				lname.toLowerCase() +
+				dob.toString().substr(2, 2)
+		) +
+		"," +
+		"@Password = " +
+		mysql.escape(md5(lname)) +
+		"," +
+		"@E_Mail =" +
+		mysql.escape(
+			fname.toLowerCase() +
+				lname.toLowerCase() +
+				dob.toString().substr(2, 2) +
+				"@gmail.com"
+		) +
+		"," +
+		"@para_out = @return_status OUTPUT";
+	// "'51'";
+	// console.log(query);
+	queryForObject(query, (err, data) => {
+		// console.log(data);
+		callback(data);
+	});
+};
+
+module.exports = queryAddNewPatient;
