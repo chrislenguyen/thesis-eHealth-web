9dea0af22f696e68820897df42172d0a7a7528bb
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2021-03-21 21:08:49 +0700

    add show current date function

--- src/views/js/home.js
+++ src/views/js/home.js
@@ -117,6 +117,19 @@ function convertLastName(lname) {
 	return allCapLName;
 }
 
+function showDate() {
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, "0");
+	var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+	var yyyy = today.getFullYear();
+
+	today = mm + "/" + dd + "/" + yyyy;
+
+	// console.log(today);
+
+	$("#currentDate").val(today);
+}
+
 // myString = "Hello World";
 // temp = [...myString];
 // array = [];
