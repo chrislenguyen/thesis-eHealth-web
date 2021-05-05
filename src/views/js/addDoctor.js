565e753b0e18bc30818547cc37ebfb1f75989a4b
chrislenguyen <34916267+chrislenguyen@users.noreply.github.com>
2021-05-05 20:44:10 +0700

    adjust exception add doctor

--- src/views/js/addDoctor.js
+++ src/views/js/addDoctor.js
@@ -1,22 +1,30 @@
 $("#submitBtn").on("click", function () {
-	var data = {};
-	$("#addDoctorForm input, #addDoctorForm select").each(function () {
-		var input = $(this);
-		var index = input.attr("id");
-		data[index] = input.val();
-	});
-	// console.log(data);
-	// console.log(JSON.stringify(data));
-	$.ajax({
-		type: "POST",
-		url: "/add-doctor",
-		data: data,
-		dataType: "json",
-		success: function (res) {
-			if (res.status == 1) {
-				$("#addDoctorForm input, select").val("");
-				$("#addSuccessDialog").show();
-			}
-		},
-	});
+	if (
+		$("#addDoctorForm :input:empty").filter(function () {
+			return $.trim($(this).val()).length == 0;
+		}).length == 0 &&
+		$("#hos").val() !== "" &&
+		$("#dep").val() !== ""
+	) {
+		var data = {};
+		$("#addDoctorForm input, #addDoctorForm select").each(function () {
+			var input = $(this);
+			var index = input.attr("id");
+			data[index] = input.val();
+		});
+		$.ajax({
+			type: "POST",
+			url: "/add-doctor",
+			data: data,
+			dataType: "json",
+			success: function (res) {
+				if (res.status == 1) {
+					$("#addDoctorForm input, select").val("");
+					$("#addSuccessDialog").show();
+				}
+			},
+		});
+	} else {
+		alert("NOT ENOUGH INPUT");
+	}
 });
